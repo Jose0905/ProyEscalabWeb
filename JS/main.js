23b7276042cbd5314aba7ba9250fda1051d2a7c9
JoseAV <jose.astorga91@hotmail.es>
2021-06-17 14:47:14 -0400

    index menu desplegable

--- JS/main.js
+++ JS/main.js
@@ -7,7 +7,7 @@ var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
     menu.style.top = "0";
   } else {
-    menu.style.top = "-100px";
+    menu.style.top = "-150px";
   }
   prevScrollpos = currentScrollPos;
 }
@@ -52,3 +52,42 @@ function validar_datos () {
   } 
 
 }
+
+var logo = document.getElementById('logo');
+var lista = document.querySelector('.lists')
+var nav = document.getElementById('menu');
+var open = document.getElementById('open');
+var close = document.getElementById('close');
+
+function esconder(){
+  let arreglo = [];
+  var lista1 = document.getElementById('lista1');
+  var lista2 = document.getElementById('lista2');
+
+  arreglo.push(lista1, lista2);
+  for(let i = 0; i < arreglo.length; i++){
+    arreglo[i].addEventListener('click', () => {
+      open.classList.remove('esconder');
+      nav.classList.remove('menuAbierto');
+      logo.classList.remove('esconder');
+      close.classList.remove('mostrarIcono');
+      lista.classList.remove('listaAbierta');
+  })
+  }
+}
+
+open.addEventListener('click', () => {
+  open.classList.add('esconder');
+  nav.classList.add('menuAbierto');
+  logo.classList.add('esconder');
+  close.classList.add('mostrarIcono');
+  lista.classList.add('listaAbierta');
+  esconder();
+});
+close.addEventListener('click', () => {
+  open.classList.remove('esconder');
+  nav.classList.remove('menuAbierto');
+  logo.classList.remove('esconder');
+  close.classList.remove('mostrarIcono');
+  lista.classList.remove('listaAbierta');
+});
