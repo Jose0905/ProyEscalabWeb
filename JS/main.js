aed65a5ec2faf9b1de8efc798d51f98186e2e747
JoseAV <jose.astorga91@hotmail.es>
2021-06-16 22:05:21 -0400

    video y formulario

--- JS/main.js
+++ JS/main.js
@@ -19,6 +19,7 @@ var ocultar = document.querySelector('#ocultar');
 
 leer_mas.addEventListener('click', () =>{
     container_nosotros.style.display = 'flex';
+    container_nosotros.style.transition = 'all 0.6s';
     leer_mas.style.display = 'none';
     ocultar.style.display = 'flex';
 })
@@ -27,3 +28,16 @@ ocultar.addEventListener('click', () =>{
     leer_mas.style.display = 'flex';
     ocultar.style.display = 'none';
 })
+
+function validar_datos () {
+  let datos = [];
+  let nombre = document.getElementById('nombre').value;
+  let email = document.getElementById('email').value;
+  let numero = document.getElementById('numero').value;
+  let mensaje = document.getElementById('mensaje').value;
+
+  
+  if(nombre != '' && email != '' &&  numero != '' && mensaje != ''){
+    alert('Gracias por contactarnos, te responederemos a la brevedad')
+  } 
+}
